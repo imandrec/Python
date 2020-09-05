a8300ba1e570e0f38479a0a2ab46d0232ddcc6c6
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-09-05 14:24:04 -0400

    Add files via upload

---
+++ password.py
@@ -0,0 +1,24 @@
+import sqlite3
+from hashlib import sha256
+
+AdminPassword = "123645"
+
+connect = input("What is your password?\n")
+
+while connect != AdminPassword:
+    connet = input("What is your password?\n")
+    if connect == "q":
+        break
+       
+conn = sqlite3.connect('pass_manager.db')
+#encode() : Converts the string into bytes to be acceptable by hash function.
+#hexdigest() : Returns the encoded data in hexadecimal format.
+def createPassword(passKey, service, adminPass):
+    return sha256(adminPass.encode('utf-8') + service.lower().encode('utf-8') + passKey.encode('utf-8')).hexdigest()[:15]
+
+def get_hex_key(adminPass, service):
+    return sha256(adminPass.encode('utf-8') + service.lower().encode('utf-8')).hexdigest()
+
+def getPassword(adminPass, service):
+    secretKey = get_hex_key(adminPass, service)
+    cursor = conn.execute("SELECT * from KEYS WHERE passKey=" + "" + secretKey + "")
