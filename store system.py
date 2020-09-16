480a9202f78b60a9d8da25bc356c12d3c2904397
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-09-16 18:16:14 -0400

    Update store system.py

--- store system.py
+++ store system.py
@@ -31,16 +31,25 @@ text_input = StringVar()
 txtDisplay = Entry(Box2,font=("arial", 20), textvariable=text_input, bd=10, insertwidth=1, bg="#FFFFFF", justify="right")
 txtDisplay.grid(columnspan=4)
 
+#show numbers on the input
 operator= ""
 def btnClick(numbers):
     global operator
     operator = operator + str(numbers)
     text_input.set(operator)
 
+#delete button
 def btnDelete():
     global operator
     operator = ""
     text_input.set("")
+
+#equal button
+def btnEqual():
+    global operator
+    sumup =str(eval(operator))
+    text_input.set(sumup)
+    operator=""
 
 #row 1 calculator
 btn7=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="7",bg="#82E0AA", command=lambda: btnClick(7)) .grid(row=2,column=0)
@@ -61,7 +70,7 @@ btn3=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="3",bg
 addition=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="+",bg="#ABEBC6", command=lambda: btnClick("+")) .grid(row=4,column=3)
 
 #row 4 calculator
-equal=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="=",bg="#ABEBC6", command=lambda: btnClick("=")) .grid(row=5,column=0)
+equal=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="=",bg="#ABEBC6", command=btnEqual) .grid(row=5,column=0)
 btn0=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="0",bg="#82E0AA", command=lambda: btnClick(0)) .grid(row=5,column=1)
 delete=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="C",bg="#ABEBC6", command=btnDelete) .grid(row=5,column=2)
 division=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="/",bg="#ABEBC6", command=lambda: btnClick("/")) .grid(row=5,column=3)
