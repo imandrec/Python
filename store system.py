d394567e7bdb6437195d1901b513729036d521b1
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-09-16 16:33:34 -0400

    Update store system.py

--- store system.py
+++ store system.py
@@ -5,22 +5,22 @@ import time;
 #To make sure that is gonna appear the canvas
 root = Tk ()
 root.geometry("1600x800")
-root.title("Store system")
+root.title("Restaurant system")
 
 Box = Frame(root, width = 1600,height = 50,bg="red", relief=SUNKEN)
 Box.pack(side=TOP)
 
-Box1 = Frame(root, width = 160,height = 700,bg="red", relief=SUNKEN)
+Box1 = Frame(root, width = 800,height = 700,bg="red", relief=SUNKEN)
 Box1.pack(side=LEFT)
 
-Box2 = Frame(root, width = 1600,height = 700,bg="red", relief=SUNKEN)
+Box2 = Frame(root, width = 300,height = 700,bg="red", relief=SUNKEN)
 Box2.pack(side=RIGHT)
 
 #Time function
 localtime=time.asctime(time.localtime(time.time()))
 
 #labels
-lblInfo = Label(Box, font=("arial",50),text="Store",fg="red", bd=10, anchor="w")
+lblInfo = Label(Box, font=("arial",50),text="Sales",fg="red", bd=10, anchor="w")
 lblInfo.grid(row=0,column=0)
 
 lblInfo = Label(Box, font=("arial",50),text=localtime,fg="red", bd=10, anchor="w")
@@ -37,6 +37,6 @@ def btnClick(numbers):
     operator = operator + str(numbers)
     text_input.set(operator)
 
-btn7=Button(Box2,padx=16,pady=16,bd=5, fg="black", font=('arial',20),text="7",bg="red", command=lambda: btnClick(7)) .grid(row=2,column=0)
+btn7=Button(Box2,padx=16,pady=16,bd=8, fg="black", font=('arial',20),text="7",bg="red", command=lambda: btnClick(7)) .grid(row=2,column=0)
 
 root.mainloop()
