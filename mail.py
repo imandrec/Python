54c0c199ad10a6f8ddaa26afda2882283ea0f1ac
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2020-10-01 18:04:49 -0700

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,4 +5,5 @@ with open("names.txt",'r',encoding = 'utf-8') as names_file:
        for name in names_file:
            mail = "Hello "+name+body
            with open(name.strip()+".txt",'w',encoding = 'utf-8') as mail_file:
-               mail_file.write(mail)
+               mail_file.do(mail)
+               
