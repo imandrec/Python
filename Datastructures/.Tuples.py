79ee000646d179c64f23010a833ee80ec406387e
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-05-25 20:41:12 -0400

    Update and rename .Tuples.py to Tuples.py

--- Datastructures/.Tuples.py
+++
@@ -1,4 +0,0 @@
-#Create a tuple
-
-tuple1 = ("disco",10,1.2 )
-tuple1
---
+++ Datastructures/Tuples.py
@@ -0,0 +1,14 @@
+#Create a tuple
+
+tuple1 = ("disco",10,1.2 )
+tuple1
+
+# Print the type of the tuple you created
+
+type(tuple1)
+
+# Print the variable on each index
+
+print(tuple1[0])
+print(tuple1[1])
+print(tuple1[2])
