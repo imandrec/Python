d375f37d478fe65b177ff275ba9d33599fc196b6
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-06-10 16:02:54 -0400

    Create week2.py

---
+++ week2.py
@@ -0,0 +1,8 @@
+#Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case.
+
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    line = line.rstrip()
+text = fh.read()
+print (text.upper())
