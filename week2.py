39614cd5375649e3d74fa5e0fdea3633f9af9803
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-06-10 16:18:13 -0400

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,8 +1,16 @@
 #Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case.
 
+#option 1
 fname = input("Enter file name: ")
 fh = open(fname)
 for line in fh:
     line = line.rstrip()
 text = fh.read()
 print (text.upper())
+
+#option 1
+fname = input("Enter file name: ")
+fh = open(fname)
+for line in fh:
+    ly = line.rstrip()
+    print(ly.upper())
