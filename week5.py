ada61df22c9011fc264800fb77bbe8b44d4f94e6
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-06-16 16:46:22 -0400

    Update week5.py

--- week5.py
+++ week5.py
@@ -4,3 +4,21 @@
 #After the dictionary is produced, the program reads through the dictionary 
 #using a maximum loop to find the most prolific committer.
 
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+counts = dict()
+for line in handle:
+   line=line.rstrip()
+   if line.startswith('From '):
+       words=line.split()
+       counts[words[1]]=counts.get(words[1],0)+1
+bigcount=0
+for word,count in counts.items():
+   if bigcount == 0 or count>bigcount:
+       bigword=word
+       bigcount=count
+
+print(bigword,bigcount)
+
