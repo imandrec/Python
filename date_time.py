081bb51811fbca7734a6cb1330081e06de1559da
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-07-10 07:21:59 -0400

    Create date_time.py

---
+++ date_time.py
@@ -0,0 +1,14 @@
+# Python3 code to demonstrate
+# Getting current date and time using
+# now().
+	
+# importing datetime module for now()
+import datetime
+	
+# using now() to get current time
+current_time = datetime.datetime.now()
+	
+# Printing value of now.
+print ("Time now at greenwich meridian is : "
+									, end = "")
+print (current_time)
