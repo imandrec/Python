093e5b2d17392d50ffab892816a1d918299db8b9
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-07-09 23:24:00 -0400

    Create product.py

---
+++ product.py
@@ -0,0 +1,17 @@
+def multiplication_or_sum(num1, num2):
+    # calculate product of two number
+    product = num1 * num2
+    # check if product is less then 1000
+    if product <= 1000:
+        return product
+    else:
+        # product is greater than 1000 calculate sum
+        return num1 + num2
+
+# first condition
+result = multiplication_or_sum(20, 30)
+print("The result is", result)
+
+# Second condition
+result = multiplication_or_sum(40, 30)
+print("The result is", result)
