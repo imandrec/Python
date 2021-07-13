e5994bd42c6842fdaaed9177b5088d17b7f5fa0c
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-07-13 15:28:54 -0400

    Update Miles-Kilometers.py

--- Miles-Kilometers.py
+++ Miles-Kilometers.py
@@ -1,8 +1,8 @@
 kilometers = 12.25
 miles = 7.38
 
-miles_to_kilometers = ###
-kilometers_to_miles = ###
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
 
 print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
 print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
