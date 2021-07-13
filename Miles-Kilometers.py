5987d5a22bd2edc66d6a7c2c56ef2d943e9f2147
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-07-13 15:09:02 -0400

    Create Miles-Kilometers.py

---
+++ Miles-Kilometers.py
@@ -0,0 +1,8 @@
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = ###
+kilometers_to_miles = ###
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
