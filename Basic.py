9590a389a964de2b69f58b7501ca52f936366f97
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-08-16 21:18:18 -0400

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -20,8 +20,7 @@ print('dog' in (findDog))
 
 seq = ['blackberries','apple','apples','blueberries','kiwi']
 print(list(filter(lambda word: word[0]=='b',seq)))
-
-#You are driving too fast, police officer stops you. Write a function to return one of 3 possible results: "No ticket", "Small ticket", or "Big Ticket". If your speed is 60 or less, the result is "No Ticket". If speed is between 61 and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all cases
+#e of 3 possible results: "No ticket", "Small ticket", or "Big Ticket". If your speed is 60 or less, the result is "No Ticket". If speed is between 61 and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big Ticket". Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all cases
 def caught_speeding(speed, is_birthday):
 
     if is_birthday:
