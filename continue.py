c82f7cee422f173dc995411d36ddb9ff93c911c2
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2021-12-13 17:53:17 -0600

    Create continue.py

---
+++ continue.py
@@ -0,0 +1,16 @@
+user_word = input("Enter your word: ")
+user_word = user_word.upper()
+
+for letter in user_word:
+    if letter == "A":
+        continue
+    elif letter == "E":
+        continue
+    elif letter == "I":
+        continue
+    elif letter == "O":
+        continue
+    elif letter == "U":
+        continue
+    else:
+        print(letter)
